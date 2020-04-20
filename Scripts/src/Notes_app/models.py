8a66749cf64ea69d1670100030f90bd2a3f8aeb3
ZakariaBrahimi <56256409+ZakariaBrahimi@users.noreply.github.com>
2020-04-20 07:29:26 +0100

    Create Note_Site Accounts app

--- Scripts/src/Notes_app/models.py
+++ Scripts/src/Notes_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+import datetime
 
 # Create your models here.
+class Note(models.Model):
+    user    = models.ForeignKey(User, on_delete=models.CASCADE)
+    title   = models.CharField(max_length=50)
+    #slug    = models.SlugField(blank=True, null=True)
+    content = models.TextField()
+    created = models.DateTimeField(blank = True, default= datetime.datetime.now)
+    active  = models.BooleanField(default=True)
+    tags    = models.CharField(blank=True, max_length = 100)
+    #img = models.ImageField(upload_to='notes_img', blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+
---
+++ Scripts/src/Notes_app/urls.py
@@ -0,0 +1,5 @@
+from django.conf.urls import url
+app_name = 'Notes_app'
+urlpatterns = {
+    
+}
